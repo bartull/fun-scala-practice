cc7bd415d891d58a606913657f6b2d02a6a7abab
Bartosz Ludwig <bartosz.ludwig@gmail.com>
2018-10-13 19:59:29 +0100

    Some minor Foldable extensions

--- src/main/scala/dogs/Functor.scala
+++ src/main/scala/dogs/Functor.scala
@@ -4,6 +4,12 @@ trait Functor[F[_]] {
 
   def map[A, B](fa: F[A])(f: A => B): F[B]
 
+  def lift[A, B](f: A => B): F[A] => F[B] = fa => map(fa)(f)
+
+  def as[A](fa: F[A])(a: A): F[A] = map(fa)(_ => a)
+
+  def mapply[A, B](a: A)(f: F[A => B]): F[B] = map(f)(_(a))
+
   def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
     (map(fab)(_._1), map(fab)(_._2))
 
@@ -19,5 +25,9 @@ object Functor {
 
   implicit val ListFunctor: Functor[List] = new Functor[List] {
     override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+
+  implicit val OptionFunctor: Functor[Option] = new Functor[Option] {
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
   }
 }
